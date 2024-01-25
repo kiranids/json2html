1c59e471d63f36a8bd2b7ea1c308c01e62909d1e
Kiran Aryal <kiran.a@idsil.loc>
2024-01-25 11:14:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,187 @@
+import React, { useState } from 'react';
+import { Modal, Button, Upload, message } from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
+
+const { Dragger } = Upload;
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [currentImage, setCurrentImage] = useState(null);
+  const [appNo, setAppNo] = useState();
+
+  const handleImageClick = (image, appNo) => {
+    setAppNo(appNo);
+    setCurrentImage(image);
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
+  const handleFileUpload = (file) => {
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      const content = event.target.result;
+      try {
+        const jsonData = JSON.parse(content);
+        setData(jsonData);
+        message.success('File uploaded successfully.');
+      } catch (error) {
+        console.error('Error parsing JSON file:', error);
+        message.error('File upload failed.');
+      }
+    };
+
+    reader.readAsText(file);
+  };
+
+  const draggerProps = {
+    name: 'file',
+    multiple: false,
+    showUploadList: false,
+    beforeUpload: (file) => {
+      handleFileUpload(file);
+      return false; // Prevent default upload behavior
+    },
+  };
+
+  return (
+    <div>
+      {/* Add File Upload */}
+      <div style={{ marginTop: '20px', marginBottom: '20px' }}>
+        <Dragger {...draggerProps}>
+          <p className="ant-upload-drag-icon">
+            <InboxOutlined />
+          </p>
+          <p className="ant-upload-text">Click or drag file to this area to upload</p>
+        </Dragger>
+      </div>
+
+      {/* Modal for Image Preview */}
+      <Modal
+        title={appNo ? `${appNo} ` : ''}
+        visible={modalVisible}
+        onCancel={closeModal}
+        footer={[
+          <Button key="back" onClick={closeModal}>
+            Close
+          </Button>,
+        ]}
+      >
+        {currentImage && <img src={`data:image/png;base64, ${currentImage}`} alt="Preview" />}
+      </Modal>
+
+      {/* Display the data if available */}
+      {data && (
+        <div>
+          <div className='flex mb-10'>
+          <div className="w-1/2 bg-yellow-300">
+  <div className="flex ">
+    <div className="w-1/2 border-1 border-black">
+      <p>
+        <strong className='px-2'>JURISDICTION CODE:</strong> {data.jurisdictionCode}
+      </p>
+    </div>
+    <div className="w-1/2 border-2 ">
+     
+    </div>
+  </div>
+  <div className="flex ">
+    <div className="w-1/2 border-2">
+    <p>
+        <strong className='px-2'>Title:</strong> {data.title}
+      </p>
+   
+    </div>
+    <div className="w-1/2 border-2 ">
+    <p>
+        <strong className='px-2'>Total Pages:</strong> {data.numberOfPages}
+      </p>
+ 
+    </div>
+  </div>
+  
+
+  <div className="flex ">
+    <div className="w-1/2 border-2">
+    <p>
+        <strong className='px-2'>Publication Date:</strong> {data.publicationDate}
+      </p>
+    </div>
+    <div className="w-1/2 border-2 ">
+      <p>
+        <strong className='px-2'>Total Records:</strong> {data.section.reduce((acc, section) => acc + section.trademarks.length, 0)}
+      </p>
+    </div>
+  </div>
+</div>
+            <div className="w1/2"></div>
+          </div>
+
+          {data.section.map((section, index) => (
+  <div key={index}>
+    <h3>
+      <h1 className="text-2xl text-gray-500 my-10">
+        {index + 1}. {section.title}
+      </h1>
+    </h3>
+
+    {section.trademarks.map((trademark, i) => (
+
+        
+
+      <div className='mb-10' key={i}>
+        <h4>
+          <strong>Trademark {i + 1}</strong>
+        </h4>
+        <p>
+          <strong>Application Number:</strong> {trademark.applicationNumber}
+        </p>
+        <p>
+          <strong>Application Date:</strong> {trademark.applicationDate}
+        </p>
+        <p>
+          <strong>Registration Number:</strong> {trademark.registrationNumber}
+        </p>
+        <p>
+          <strong>Verbal Elements:</strong>{' '}
+          {trademark.verbalElements && trademark.verbalElements.join(',')}
+        </p>
+
+        <div className=' flex'>
+          <Button
+            className='bg-gray-400'
+            onClick={() => handleImageClick(trademark.deviceElements[0], trademark.applicationNumber)}
+          >
+            View Trademark Device
+          </Button>
+
+          {trademark?.clippings?.map((clipping, j) => (
+            <div key={j}>
+              {clipping?.binaryContent && (
+                
+                <Button className='bg-gray-400 hover:text-white' onClick={() => handleImageClick(clipping.binaryContent, trademark.applicationNumber)}> View Clipping Image </Button>
+              )}
+            </div>
+          ))}
+        </div>
+        {i < section.trademarks.length - 1 && <hr />}
+      </div>
+
+    ))}
+  </div>
+))}
+
+          {/* Display additional content based on the uploaded data */}
+          {/* <h3>Data from Uploaded JSON:</h3>
+          <pre>{JSON.stringify(data, null, 2)}</pre> */}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
